ad635ee70c4a8adf271b775713beb9aa231edd8b
Peter <63479070+peterb2396@users.noreply.github.com>
2023-09-20 00:47:43 -0400

    fixes

--- python/hw1/hw1.py
+++ python/hw1/hw1.py
@@ -1,6 +1,8 @@
 import numpy as np
 import time
 
+debug = False
+
 def load_data(path):
     try:
         with open(path, 'r') as file:
@@ -49,10 +51,11 @@ def cov_1(data):
             E[i, j] = cov_ij
             E[j, i] = cov_ij
 
+    if debug:
+        finish_time = time.time()
+        duration = (finish_time - start_time) * 1000 
+        print(f"cov_1 took {duration}ms")
 
-    finish_time = time.time()
-    duration = (finish_time - start_time) * 1000  # MS
-    print(f"cov_1 took {duration}ms")
     return E
 
 # as a matrix product, O(d)
@@ -61,10 +64,12 @@ def cov_2(data):
 
     # Multiple the matrices
     E = np.dot(data.T, data) / len(data)
+    
+    if debug:
+        finish_time = time.time()
+        duration = (finish_time - start_time) * 1000 
+        print(f"cov_2 took {duration}ms")
 
-    finish_time = time.time()
-    duration = (finish_time - start_time) * 1000 
-    print(f"cov_2 took {duration}ms")
     return E
 
 # as sum of outer products, O(d^3)
@@ -81,41 +86,30 @@ def cov_3(data):
 
     # Multiply by 1/n
     E = sum/len(data)
+    if debug:
+        finish_time = time.time()
+        duration = (finish_time - start_time) * 1000
+        print(f"cov_3 took {duration}ms")
 
-    finish_time = time.time()
-    duration = (finish_time - start_time) * 1000
-    print(f"cov_3 took {duration}ms")
     return E
 
 # Find optimal dimension, r, to reduce D to that keeps a ratio of variance
+# Calculates retention for each dimension until we find one suitable for our needs (most optimal)
 def find_r(Y, a):
 
     # store original dimension
     d = Y.shape[0]
 
-    # Calculate the original variance to compute ratio f(r)
-    og_var = 0
-    for i in range(d): 
-        y = Y[i]
-        og_var += y
     # Compute f(r) for each 1 <= r < d until f(r) >= a
     # Break when we find so that we dont waste memory
     for r in range(1, d):
 
-        # Sum the first r eigen values: Projected Variance
-        proj_var = 0
-        for i in range(r):
-            y = Y[i]
-            proj_var += y
-        
-        # Calculate realized variance
-        var_ratio = proj_var / og_var
-
         # Success if this ratio is >= a (executes at most once)
-        if (var_ratio >= a):
+        if (calculate_retention(Y, r) >= a):
+            
             # Determine the number of principal components (PCs) r that will ensure 100a% retained variance
             print("There are %d PCs that maintain at least %.5f ratio of variance" % (d - r + 1, a))
-            print("The provided matrix was reduced to %d %s with an accuracy of %.5f" % (r, ("dimensions" if r > 1 else "dimension"), var_ratio))
+
             return r
         
     # PCA Fails
@@ -126,40 +120,73 @@ def find_r(Y, a):
 # PCA Function for this data and alpha value
 def PCA(D, a):
     # Proceed if successful load of file, otherwise try the next input (or finish)
-        if D.any():
-            # center the data as a new matrix, Z
-            Z = center(D)
-            E = cov_2(Z)
-            Y, U = np.linalg.eig(E) # Eigen values = _, eigen vectors = U
+    if D.any():
+        # center the data as a new matrix, Z
+        Z = center(D)
+        E = cov_2(Z)
+        Y = np.linalg.eigvals(E) # Eigen values = Y
 
-            r = find_r(Y, a)
+        # Find optimal r for this alpha value
+        r = find_r(Y, a)
 
-            if (r == -1):
-                # Not possible to reduce dimension based on alpha
-                return np.empty([1,1])
+        if (r == -1):
+            # Not possible to reduce dimension based on alpha
+            return np.empty([1,1])
 
-
-            # We can reduce to r dimensions! Proceed:
-
-            n, d = D.shape # dimensions of the data
-
-            # initialize result matrix, nxr
-            A = np.zeros((n, r))
-            U_r = U[:, :r] # Consider first r eigen vectors (cols)
-
-            # for each data point (row)..
-            for i in range(n):
-                d = D[i].reshape(1, -1).T
+        # Reduce to r dimensions
+        return reduce(D, r)
         
-                # New data sample (row) is U^T (row) * original data sample (col)
-                A[i, :] = np.dot(U_r.T, d) # result must be 1xr. 
-            
-            # return the reduced dimensionality array!
-            return A
                 
             
+# Reduce the given data to a specified number of dimensions.
+# PCA will find this r for us, or (for homework) we can skip straight here and choose r
+def reduce(D, r):
+    # Proceed if successful load of file, otherwise try the next input (or finish)
+    if D.any():
+        # center the data as a new matrix, Z
+        Z = center(D)
+        E = cov_2(Z)
+        Y, U = np.linalg.eig(E) # Eigen values = Y, eigen vectors = U
 
+        # Retention statistic display
+        print("Reducing to %d %s maintains accuracy ratio of %.5f" % (r, ("dimensions" if r > 1 else "dimension"),calculate_retention(Y, r)))
+
+        n, d = D.shape # dimensions of the data
+
+        # initialize result matrix, nxr
+        A = np.zeros((n, r))
+        U_r = U[:, :r] # Consider first r eigen vectors (cols)
+
+        # for each data point (row)..
+        for i in range(n):
+            d = D[i].reshape(1, -1).T
+            #A[i, :].reshape(A[i, :].shape[0], r)
+            # New data sample (row) is U^T (row) * original data sample (col)
+            A[i, :] = np.dot(U_r.T, d).T # result must be 1xr. 
+        
+        # return the reduced dimensionality array!
+        return A
                 
+def calculate_retention(Y, r):
+    # Original dimension, d:
+    d = Y.shape[0]
+
+    # Calculate the original variance to compute ratio f(r)
+    og_var = 0
+    for i in range(d): 
+        y = Y[i]
+        og_var += y
+    
+
+    # Sum the first r eigen values: Projected Variance
+    proj_var = 0
+    for i in range(r):
+        y = Y[i]
+        proj_var += y
+    
+    # Calculate realized variance
+    return( proj_var / og_var )
+
 
 
 
@@ -169,7 +196,7 @@ if __name__ == '__main__':
     # For each dataset, process the following
     for input in inputs:
         data = load_data(input)
-        A = PCA(data, 0.9)
+        A = PCA(data, 0.999)
 
         if(A.any()):
             print(A)
